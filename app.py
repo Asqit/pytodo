6090984945d379038b1f00ef9ddd89ea2abf3b2c
Ondřej Tuček <ondrejtucek9@gmail.com>
2024-02-08 20:30:34 +0100

    more work

---
+++ app.py
@@ -0,0 +1,53 @@
+import tkinter as tk
+import tkinter.font as tkFont
+import todo
+import scrollableContainer
+
+
+class App:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("PyTodo")
+        self.dimensions = (360, 500)
+
+        screenwidth = root.winfo_screenwidth()
+        screenheight = root.winfo_screenheight()
+        align_str = "%dx%d+%d+%d" % (
+            self.dimensions[0],
+            self.dimensions[1],
+            (screenwidth - self.dimensions[0]) / 2,
+            (screenheight - self.dimensions[1]) / 2,
+        )
+
+        self.root.geometry(align_str)
+        self.root.resizable(width=False, height=False)
+
+        self.todo_items = []
+
+        ft = tkFont.Font(family="Arial", size=10)
+
+        self.todo_box = scrollableContainer.ScrollableContainer(self.root)
+        self.todo_box.pack(expand=True, fill=tk.BOTH)
+
+        self.input_box = tk.Entry(root)
+        self.input_box["borderwidth"] = "1px"
+        self.input_box["relief"] = tk.FLAT
+        self.input_box["font"] = ft
+        self.input_box.place(x=10, y=460, width=240, height=30)
+
+        submit_button = tk.Button(root)
+        submit_button["bg"] = "#0d9467"
+        submit_button["font"] = ft
+        submit_button["relief"] = tk.FLAT
+        submit_button["fg"] = "#ffffff"
+        submit_button["justify"] = "center"
+        submit_button["text"] = "save"
+        submit_button.place(x=260, y=460, width=83, height=30)
+        submit_button["command"] = self.save_todo
+
+    def save_todo(self):
+        value = self.input_box.get()
+
+        if value:
+            item = todo.Todo(self.todo_box.interior, value)
+            self.input_box.delete(0, tk.END)
---
+++ todo.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+
+
+def strike(text):
+    result = ''
+    for c in text:
+        result = result + c + '\u0336'
+    return result
+
+class Todo:
+    def __init__(self, master, text):
+        self.master = master
+        self.text = text
+        self.checked = tk.BooleanVar(value=False)
+
+        self.frame = tk.Frame(master)
+        self.frame.pack(fill=tk.X)
+
+        self.checkbox = tk.Checkbutton(
+            self.frame, variable=self.checked, command=self.toggle_strike
+        )
+        self.checkbox.pack(side=tk.LEFT)
+
+        self.label = tk.Label(self.frame, text=self.text, font=("Arial", 12))
+        self.label.pack(side=tk.LEFT, padx=5)
+
+        self.delete_button = tk.Button(self.frame, text="Delete", command=self.delete)
+        self.delete_button.pack(side=tk.RIGHT)
+
+    def toggle_strike(self):
+        if self.checked.get():
+            self.label.config(text=strike(self.text))
+        else:
+            self.label.config(text=self.text)
+
+    def delete(self):
+        self.frame.destroy()
